462e89287f5def3139d463451e6976532368eb87
Ilia Popov <ilia_popov02@abv.bg>
2023-05-18 14:49:27 +0300

    Add all classes functionalities in one file

--- Template/src/main.js
+++ Template/src/main.js
@@ -1,1 +1,15 @@
+import { bookGenre } from './common/book-genre.js';
+import { libraryShelf } from './common/library-shelf.js';
+import { Book } from './models/book.js';
+import { Library } from './models/library.js';
 
+const book1 = new Book('IT', bookGenre.Horror, 1138, 'Steven King');
+const book2 = new Book('Startide Rising', bookGenre.SciFi, 124, 'David Brin');
+
+const library = new Library();
+
+library.addBook(book1, libraryShelf.Clowns);
+library.addBook(book2, libraryShelf.Unclassified);
+
+
+console.log(library.printBooks());
