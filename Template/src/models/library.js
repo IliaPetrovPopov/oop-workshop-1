bd70ff736a058ccc7edd7ff9ef90fd148d716265
Ilia Popov <ilia_popov02@abv.bg>
2023-05-19 07:50:49 +0300

    Add _shelves prop and addBook() method in Library

--- Template/src/models/library.js
+++ Template/src/models/library.js
@@ -1,5 +1,16 @@
 import { libraryShelf } from '../common/library-shelf.js';
 
 export class Library {
+    _shelves = {};
 
+  addBook(book, shelf) {
+    if (!Object.values(libraryShelf).includes(shelf)) {
+      throw new Error('Such library shelf does not exist!');
+    }
+
+    if (!this._shelves.hasOwnProperty(shelf)) {
+      this._shelves[shelf] = [];
+    }
+    this._shelves[shelf].push(book);
+  }
 }
