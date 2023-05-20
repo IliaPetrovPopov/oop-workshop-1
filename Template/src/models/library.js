b4ef9ee6613ffd311d61e78e2c26840b399b6831
Ilia Popov <ilia_popov02@abv.bg>
2023-05-20 08:40:58 +0300

    Add printBooks() method in library.js

--- Template/src/models/library.js
+++ Template/src/models/library.js
@@ -13,4 +13,23 @@ export class Library {
     }
     this._shelves[shelf].push(book);
   }
+
+  printBooks() {
+    if (Object.values(this._shelves).every(shelf => !shelf.length)) {
+      return ('No books added');
+    }
+
+    let existingShelves = '';
+    // eslint-disable-next-line guard-for-in
+    for (const shelf in this._shelves) {
+
+      const currShelfBooks = this._shelves[shelf].reduce((books, curr) => {
+        return books += `${curr.title}, by ${curr.authors}, ${curr.pages} pages\n`;
+      }, `  == ${libraryShelf[shelf]} ==\n`);
+
+      existingShelves += `${currShelfBooks}\n`;
+    }
+
+    return existingShelves;
+  }
 }
