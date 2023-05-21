9f4862cefb6eace135c7dfee706add9c70852e7a
Ilia Popov <ilia_popov02@abv.bg>
2023-05-21 11:35:51 +0300

    Re-position everything inside library.js

--- Template/src/models/library.js
+++ Template/src/models/library.js
@@ -3,33 +3,33 @@ import { libraryShelf } from '../common/library-shelf.js';
 export class Library {
     _shelves = {};
 
-  addBook(book, shelf) {
-    if (!Object.values(libraryShelf).includes(shelf)) {
-      throw new Error('Such library shelf does not exist!');
+    addBook(book, shelf) {
+      if (!Object.values(libraryShelf).includes(shelf)) {
+        throw new Error('Such library shelf does not exist!');
+      }
+
+      if (!this._shelves.hasOwnProperty(shelf)) {
+        this._shelves[shelf] = [];
+      }
+      this._shelves[shelf].push(book);
     }
 
-    if (!this._shelves.hasOwnProperty(shelf)) {
-      this._shelves[shelf] = [];
+    printBooks() {
+      if (Object.values(this._shelves).every(shelf => !shelf.length)) {
+        return ('No books added');
+      }
+
+      let existingShelves = '';
+      // eslint-disable-next-line guard-for-in
+      for (const shelf in this._shelves) {
+
+        const currShelfBooks = this._shelves[shelf].reduce((books, curr) => {
+          return books += `${curr.title}, by ${curr.authors}, ${curr.pages} pages\n`;
+        }, `  == ${libraryShelf[shelf]} ==\n`);
+
+        existingShelves += `${currShelfBooks}\n`;
+      }
+
+      return existingShelves;
     }
-    this._shelves[shelf].push(book);
-  }
-
-  printBooks() {
-    if (Object.values(this._shelves).every(shelf => !shelf.length)) {
-      return ('No books added');
-    }
-
-    let existingShelves = '';
-    // eslint-disable-next-line guard-for-in
-    for (const shelf in this._shelves) {
-
-      const currShelfBooks = this._shelves[shelf].reduce((books, curr) => {
-        return books += `${curr.title}, by ${curr.authors}, ${curr.pages} pages\n`;
-      }, `  == ${libraryShelf[shelf]} ==\n`);
-
-      existingShelves += `${currShelfBooks}\n`;
-    }
-
-    return existingShelves;
-  }
 }
